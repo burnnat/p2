6bfdcbe996b26ebe8dfebbe65eab2e014d93d199
Nat Burns <nbaccount@burnskids.com>
2013-12-11 00:01:38 -0500

    Set up project to combine artifacts from staging area.

---
+++ stage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+STAGING_DIR="${PROJECT_DIR}/staging/${TRAVIS_REPO_SLUG}"
+
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+
+cd $PROJECT_DIR
+if [ -e $STAGING_DIR ]
+then
+    git rm -r "${STAGING_DIR}"
+fi
+
+cp -r "$1" "${STAGING_DIR}"
+git add "staging/${TRAVIS_REPO_SLUG}"
+
+git commit -m "Adding artifacts from ${TRAVIS_REPO_SLUG} build ${TRAVIS_BUILD_ID}"
+git push origin $TRAVIS_BRANCH
