b13030596cfbd95eb03a1698e168f47ae64e49a5
Nat Burns <nbaccount@burnskids.com>
2013-12-13 21:17:39 -0500

    Fix staging issues.

--- stage.sh
+++ stage.sh
@@ -5,8 +5,11 @@ SOURCE_DIR="${TRAVIS_BUILD_DIR}/$1"
 
 cd $PROJECT_DIR
 
+git config user.name "${GIT_NAME}"
+git config user.email "${GIT_EMAIL}"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+
 git remote add origin https://github.com/burnnat/p2
 
 if [ -e $STAGING_DIR ]
@@ -14,8 +17,9 @@ then
     git rm -r "${STAGING_DIR}"
 fi
 
+mkdir -p "${STAGING_DIR}"
 cp -r "${SOURCE_DIR}" "${STAGING_DIR}"
 git add "${STAGING_DIR}"
 
 git commit -m "Adding artifacts from ${TRAVIS_REPO_SLUG} build ${TRAVIS_BUILD_ID}"
-git push origin $TRAVIS_BRANCH
+git push origin master
