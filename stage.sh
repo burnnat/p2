0a3d6043e6964925ec43d273612782cfb4ff0584
Nat Burns <nbaccount@burnskids.com>
2013-12-13 21:34:03 -0500

    Remove duplicate remote, add debug output.

--- stage.sh
+++ stage.sh
@@ -3,14 +3,19 @@ PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 STAGING_DIR="${PROJECT_DIR}/staging/${TRAVIS_REPO_SLUG}"
 SOURCE_DIR="${TRAVIS_BUILD_DIR}/$1"
 
+echo "Project directory: ${PROJECT_DIR}"
+echo "Staging directory: ${STAGING_DIR}"
+echo "Source directory: ${SOURCE_DIR}"
+echo "Project directory: ${PROJECT_DIR}"
+echo "Username: ${GIT_NAME}"
+echo "User email: ${GIT_EMAIL}"
+
 cd $PROJECT_DIR
 
 git config user.name "${GIT_NAME}"
 git config user.email "${GIT_EMAIL}"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
-
-git remote add origin https://github.com/burnnat/p2
 
 if [ -e $STAGING_DIR ]
 then
